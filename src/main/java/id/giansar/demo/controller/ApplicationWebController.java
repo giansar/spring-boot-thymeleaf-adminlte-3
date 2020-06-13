896686c5257bde38ca41f1df3a01c1a7af892e20
Giansar Pratiknya <giansar@users.noreply.github.com>
2020-06-14 03:04:00 +0700

    added Controller

---
+++ src/main/java/id/giansar/demo/controller/ApplicationWebController.java
@@ -0,0 +1,27 @@
+/*
+ * Spring Boot Demo with Thymeleaf and AdminLTE 3
+ * https://github.com/giansar/spring-boot-thymeleaf-adminlte-3
+ */
+package id.giansar.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Giansar
+ */
+@Controller
+public class ApplicationWebController {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationWebController.class);
+
+    @RequestMapping("/")
+    public String getIndex(Model model) {
+        LOG.info("Index");
+        return "index";
+    }
+}
